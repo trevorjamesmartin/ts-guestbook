6e23746d702d08377f934fdcebdc43ccdddd24c0
Trevor Martin <trevor.j.martin@gmail.com>
2022-04-12 23:15:24 -0400

    populate with default users

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import app from './src/api/server';
 
 app.listen(3000, () => {
-    console.log('listening on port 3000');
+    console.log('listening @ http://127.0.0.1:3000');
 });
 
---
+++ src/data/seeds/users.ts
@@ -0,0 +1,19 @@
+import { Knex } from "knex";
+import bcrypt from 'bcryptjs';
+
+function genUser (name:string, password:string) {
+    return {username: name, password: String(bcrypt.hashSync(password, 13))}
+}
+
+export async function seed(knex: Knex): Promise<void> {
+    // Deletes ALL existing entries
+    await knex("users").del();
+    let pass = process.env.PW_STRING || "password"
+    const users = [
+        ["admin", `${pass}`],
+        ["user1", `${pass}1`],
+        ["user2", `${pass}2`]
+    ].map(([a, b]) => genUser(a, b))
+    // Inserts seed entries
+    await knex("users").insert(users);
+};
