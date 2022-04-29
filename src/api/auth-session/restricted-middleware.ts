0eb43f4adbf867b0bf54b855b8e106b70139baed
Trevor Martin <trevor.j.martin@gmail.com>
2022-04-29 12:00:21 -0400

    update session middleware

--- src/api/auth-session/restricted-middleware.ts
+++ src/api/auth-session/restricted-middleware.ts
@@ -1,7 +1,16 @@
-export default (req:any, res:any, next:any) => {
-    if (req.session && req.session.loggedIn) {
-        next();
-    } else {
-        res.redirect('http://localhost:3000/logout');
+import Profiles from '../users/profile-model';
+
+export default async (req:any, res:any, next:any) => {
+    const sessionExists = (req.session && req.session.loggedIn && req.session.username);
+    if (!sessionExists) {
+        console.log('no existing session, development mode only')
+        return res.redirect('/logout');        
     }
+    console.log(req.session)
+    let profile = await Profiles.findByUsername(req.session.username);
+    if (!profile) {
+        console.log('no profile for', req.session.username);
+        return res.redirect('/logout');
+    }
+    next();
 }
