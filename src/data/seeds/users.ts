b171ca6af4c1a63d63d4f55c76058ca8254d4ad4
Trevor Martin <trevor.j.martin@gmail.com>
2022-04-29 12:03:50 -0400

    update seeds

--- src/data/seeds/users.ts
+++ src/data/seeds/users.ts
@@ -10,11 +10,13 @@ export async function seed(knex: Knex): Promise<void> {
     await knex("users").del();
     let pass = process.env.PASSWORD || "password"
     const users = [
-        ["admin", `${pass}`]
+        ["admin", `${pass}`],
+        ["development", `dev-${pass}`]
     ].map(([a, b]) => genUser(a, b))
     // Inserts seed entries
     await knex("users").insert(users);
     await knex("profiles").insert([
-        {user_id: 1}
+        {user_id: 1},
+        {user_id: 2}
     ]);
 };
