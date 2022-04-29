4777732d6a9a5f1667687d43727bf4b9ebe246b6
Trevor Martin <trevor.j.martin@gmail.com>
2022-04-29 15:30:34 -0400

    authorization

--- src/app/src/features/api.ts
+++ src/app/src/features/api.ts
@@ -2,12 +2,20 @@ import axios from 'axios';
 
 // const DEV_BASE = "http://localhost:3000";
 
-const api = axios.create({
-    // baseURL: process.env.REACT_APP_BASE_URL || DEV_BASE,
-    withCredentials: true
-});
+const api = (token:string|undefined) => {
+    if (!token) {
+        return axios.create({
+            // baseURL: process.env.REACT_APP_BASE_URL || DEV_BASE,
+            withCredentials: true,
+        });
+    }    
+    return axios.create({
+        // baseURL: process.env.REACT_APP_BASE_URL || DEV_BASE,
+        withCredentials: true,
+        headers: {
+            "Authorization": token
+        }
+    });
+}
 
-// const withCredentials = (api) => {
-//     api.
-// }
 export default api;
