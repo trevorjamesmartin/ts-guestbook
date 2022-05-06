919439c83b818edf75cfbe0e4f90c8541a86e89f
Trevor Martin <trevor.j.martin@gmail.com>
2022-05-05 22:49:08 -0400

    sort feed

--- src/api/feed/feed-router.ts
+++ src/api/feed/feed-router.ts
@@ -2,10 +2,15 @@ import { Router } from "express";
 import feedModel from "./feed-model";
 const router = Router();
 
-router.get('/', async (req:any, res) => {
-    let {decodedToken} = req;
-    let result = await feedModel.mainFeed(decodedToken);
-    return res.status(200).json(result);
+router.get('/', async (req: any, res) => {
+    let { decodedToken } = req;
+    function ascending(y:any, x:any) {
+        return y.id - x.id
+    }
+    function descending(y:any, x:any) {
+        return x.id - y.id
+    }
+    return res.status(200).json((await feedModel.mainFeed(decodedToken)).sort(descending));
 });
 
 export default router;
