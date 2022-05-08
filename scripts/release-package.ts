79487bd5515421e03b4a985343675e43ac44bb6d
Trevor Martin <trevor.j.martin@gmail.com>
2022-05-08 16:09:38 -0400

    containerize

--- scripts/release-package.ts
+++ scripts/release-package.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import fs from 'fs';
+
 // NODE
 const packageJson = require('../package.json');
 let packageDist = {...packageJson,
@@ -16,7 +17,6 @@ let filename = path.join(process.cwd(), 'dist', 'package.json');
 fs.writeFileSync(filename, data);
 
 // DOCKER
-const NODE_IMAGE="18-alpine"
 const WORKDIR="/opt/app";
 const RELEASE_FILE="dist.tar.gz";
 const REMOVE_CONTENT="mv dist/* . && rmdir dist";
@@ -24,16 +24,24 @@ const ENV_PASSWORD="s3cr3tP4ssw0rd";
 const PORT=8080
 let dockerfile = path.join(process.cwd(), 'release', 'Dockerfile');
 
-fs.appendFileSync(dockerfile, `FROM node:${NODE_IMAGE}\n`);
-fs.appendFileSync(dockerfile, `RUN apk add g++ make python3\n`);
+fs.writeFileSync(dockerfile, `
+FROM node:17-alpine
 
-fs.appendFileSync(dockerfile, `WORKDIR ${WORKDIR}\n`);
-// copy latest release file
-fs.appendFileSync(dockerfile, `COPY ${RELEASE_FILE} .\n`);
-// extract contents
-fs.appendFileSync(dockerfile, `RUN tar zxvf ${RELEASE_FILE} && ${REMOVE_CONTENT} && rm ${RELEASE_FILE}\n`);
-fs.appendFileSync(dockerfile, `ENV PASSWORD=${ENV_PASSWORD}\n`);
-// install library dependencies
-fs.appendFileSync(dockerfile, `RUN npm install && mkdir ${WORKDIR}/db && npm run create:db\n`);
-fs.appendFileSync(dockerfile, `EXPOSE ${PORT}\n`);
-fs.appendFileSync(dockerfile, `CMD ["npm", "start"]\n`);
+WORKDIR ${WORKDIR}
+COPY ${RELEASE_FILE} .
+ENV PASSWORD=${ENV_PASSWORD}
+
+RUN tar zxvf ${RELEASE_FILE} \
+    && ${REMOVE_CONTENT} \
+    && rm ${RELEASE_FILE}    
+
+RUN apk add --no-cache --virtual .gyp python3 make g++ \
+    && npm install \
+    && mkdir ${WORKDIR}/db \
+    && npm run create:db \
+    && apk del .gyp
+
+EXPOSE ${PORT}
+
+CMD ["node", "index.js"]
+`);
