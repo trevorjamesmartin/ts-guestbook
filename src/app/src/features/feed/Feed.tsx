f40b4e827cbde614e756bcfd66d8d49dbbf01ff8
Trevor Martin <trevor.j.martin@gmail.com>
2022-05-08 03:24:00 -0400

    update feed

--- src/app/src/features/feed/Feed.tsx
+++ src/app/src/features/feed/Feed.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useAppSelector, useAppDispatch } from "../../memory/hooks";
 
-// import { BlogPost, selectors as postsSelectors, getPostsAsync, submitPostAsync, actions as postsActions } from './postsSlice';
+import { postsStore, BlogPost, selectors as postsSelectors, getPostsAsync, submitPostAsync, actions as postsActions } from '../posts/postsSlice';
 import { getFeedAsync, selectors as feedSelectors, actions as feedActions, Food } from './feedSlice';
+import { selectors as authSelectors } from '../auth/authSlice'
 
-// import { selectors as profileSelectors, getProfileAsync } from '../profile/profileSlice';
 import { usersAsync, selectors as userSelectors } from '../users/userSlice';
 
 
@@ -14,7 +14,11 @@ import {
 } from 'reactstrap';
 const { selectFeed } = feedSelectors;
 const { selectList } = userSelectors;
+const { selectToken } = authSelectors;
+
 const { clear } = feedActions;
+const { selectCurrent } = postsSelectors;
+const { setCurrent } = postsActions;
 
 const LiteralFood = (props: Partial<Food>) => {
   return (
@@ -31,33 +35,48 @@ const LiteralFood = (props: Partial<Food>) => {
 
 function Feed() {
   const { food: socialFood, status } = useAppSelector(selectFeed);
+  const token = useAppSelector(selectToken);
+  const authorized = token && token.length > 4;
+
   const [socialFeed, setSocialFeed] = useState([...socialFood])
   const dispatch = useAppDispatch();
-  // const currentPost = useAppSelector(selectCurrent);
+  const currentPost: BlogPost = useAppSelector(selectCurrent);
   useEffect(() => {
     dispatch(getFeedAsync());
-    console.log(status, socialFeed);
+
   }, []);
   const handleSubmitForm = (e: any) => {
     e.preventDefault();
-    // console.log(currentPost);
-    // dispatch(submitPostAsync());
+    console.log(currentPost);
+    dispatch(submitPostAsync());
+    setTimeout(() => {
+      dispatch(getFeedAsync());
+    }, 500);
   }
   const handleChange = (e: any) => {
-    // let name: string = e.currentTarget.name;
-    // let value: any = e.target.value;
-    // dispatch(setCurrent({ [name]: value }));
+    let name: string = e.currentTarget.name;
+    let value: any = e.target.value;
+    dispatch(setCurrent({ [name]: value }));
   }
   return (<div className="Posts">
-    {/* <Form onSubmit={handleSubmitForm} className="blog-post">
-            <Input value={currentPost.title} onChange={handleChange} name="title" type="text" placeholder="title: subject" />
-            <Input value={currentPost.tags} onChange={handleChange} name="tags" type="text" placeholder="tags: general,random,blog" />
-            <Input placeholder="What's happening?" type="textarea" value={currentPost?.content} onChange={handleChange} name="content" />
-            <Button>post</Button>
-        </Form> */}
-    <ul>
-      {socialFood.map((f: Partial<Food>) => LiteralFood(f))}
-    </ul>
+    {authorized ?
+      <>
+        <Form onSubmit={handleSubmitForm} className="blog-post">
+          {/* <Input value={currentPost.title} onChange={handleChange} name="title" type="text" placeholder="title: subject" /> */}
+          {/* <Input value={currentPost.tags} onChange={handleChange} name="tags" type="text" placeholder="tags: general,random,blog" /> */}
+          <Input placeholder="What's happening?" type="textarea" value={currentPost?.content} onChange={handleChange} name="content" />
+          <Button>post</Button>
+        </Form>
+        <ul>
+          {socialFood.map((f: Partial<Food>) => LiteralFood(f))}
+        </ul>
+      </> :
+      <>
+      <h2>good times</h2>
+      <p>login to continue</p>
+      <p>first time here? Register for a new account.</p>
+      </>
+    }
   </div>)
 }
 export default Feed;
