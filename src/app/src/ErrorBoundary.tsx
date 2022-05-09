36b1b6e32ff506f965aae6b57fbd07a19a0f1cdb
Trevor Martin <trevor.j.martin@gmail.com>
2022-05-09 01:42:35 -0400

    error boundary

---
+++ src/app/src/ErrorBoundary.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+class ErrorBoundary extends React.Component {
+  state = {
+    hasError: false
+  }
+  constructor(props:any) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error:any) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error:any, errorInfo:any) {
+    // You can also log the error to an error reporting service
+    console.log({error, errorInfo});
+    // setTimeout(() => {
+    //   window.location.reload();
+    // }, 1700);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h4>Something went wrong... reloading</h4>;
+    }
+
+    return this.props.children; 
+  }
+}
+
+export default ErrorBoundary;
