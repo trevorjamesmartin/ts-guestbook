4b58700eb6877f0217fa4b4d2cbed927cf66b556
Trevor Martin <trevor.j.martin@gmail.com>
2022-05-11 13:55:13 -0400

    use NODE_ENV

--- src/data/dbConfig.ts
+++ src/data/dbConfig.ts
@@ -1,7 +1,7 @@
 const knex = require('knex');
 const config = require('../../knexfile');
 
-const environment = "development";
+const environment = process.env.NODE_ENV||"development";
 
 export default knex(config[environment]);
 
