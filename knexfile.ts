ea79370f04c456c110e28d23d059f2bd705c0d9f
Trevor Martin <trevor.j.martin@gmail.com>
2022-05-11 13:58:55 -0400

    cleanup

--- knexfile.ts
+++ knexfile.ts
@@ -3,39 +3,21 @@ import type { Knex } from "knex";
 // Update with your config settings.
 
 const config: { [key: string]: Knex.Config } = {
-  old_development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./db/dev.sqlite3"
-    },
-    useNullAsDefault: true,
+  development: {
+    client: "pg",
+    connection: "postgresql://vigilant:cloud@127.0.0.1:5432/vigilant-cloud",
     migrations: {
-      directory: "./src/data/migrations"
+      directory: "./src/data/migrations",
+      tableName: "migrations"
     },
     seeds: {
       directory: "./src/data/seeds"
     }
   },
 
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-
-  development: {
+  production: {
     client: "pg",
-    connection: process.env.DATABASE || "postgresql://vigilant:cloud@127.0.0.1:5432/vigilant-cloud",
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./src/data/migrations",
       tableName: "migrations"
