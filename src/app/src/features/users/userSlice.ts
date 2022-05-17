5c612404caa3a24ae62452b53f7fc0bf226148e1
Trevor Martin <trevor.j.martin@gmail.com>
2022-05-17 16:51:30 -0400

    update url params

--- src/app/src/features/users/userSlice.ts
+++ src/app/src/features/users/userSlice.ts
@@ -1,11 +1,12 @@
 import { createSlice, PayloadAction, createAsyncThunk } from '@reduxjs/toolkit';
 import api from '../network/api';
 import { RootState } from '../../memory/store'
-import { Paginated } from '../Pagination';
+import { Paginated } from '../pagination';
 
 interface UserList extends Paginated {
   status: string;
 }
+
 const clearState: UserList = {
   pages: [],
   next: undefined,
@@ -15,47 +16,24 @@ const clearState: UserList = {
 
 export const usersAsync = createAsyncThunk(
   'users/list',
-  async (_, thunkAPI) => {
+  async (params: any, thunkAPI) => {
     const state: any = thunkAPI.getState();
-    const token = state?.auth?.token || undefined;
+    const token = state?.auth?.token;
     const apiClient = new api(token);
-    const response = await apiClient.get('/api/users/with-profiles'); // pending
-    console.log(response)
+    const { protocol, pathname, host } = window.location;
+    let searchString = '';
+    const { page } = params;
+    let response: any = !page ?
+      await apiClient.get('/api/users/with-profiles', { params: { page: 1, limit: 9 } }) :
+      await apiClient.get('/api/users/with-profiles', { params: { page, limit: 9 } });
+    // console.log(response)
+    if (page > 1 && response.status === 200) {
+      searchString = `?page=${page}`;
+    }
+    let url = `${protocol}//${host}${pathname}${searchString}`;
+    window.history.pushState({}, '', url);
     return response.data; // fulfilled
   }
-);
-export const usersNextPageAsync = createAsyncThunk(
-  'users/page-next',
-  async (key: string, thunkAPI) => {
-    const state: any = thunkAPI.getState();
-    const token = state?.auth?.token || undefined;
-    let rPath:string = '/api/users/with-profiles/';
-    let pg;
-    switch (key) {
-      case 'next':
-        pg = state.users.next.page;
-        break;
-      case 'previous':
-        pg = state.users.previous.page;
-        break;
-      default:
-        break;
-    }
-    if (pg) {
-      rPath += `?page=${pg}`;
-    } else {
-      return state.users;
-    }
-    let apiClient = new api(token);
-    console.log(key, rPath);
-    const response = await apiClient.get(rPath); // pending
-    if(response.status === 200) {
-      return response.data; // fulfilled
-    }
-    console.log(response.status, rPath);
-    return state.users;
-  }
-
 );
 
 export const userSlice = createSlice({
@@ -67,7 +45,7 @@ export const userSlice = createSlice({
       state.pages = clearState.pages;
       state.next = clearState.next;
       state.previous = clearState.previous;
-    }
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(usersAsync.pending, (state) => {
@@ -83,31 +61,19 @@ export const userSlice = createSlice({
         state.status = 'failed';
         state.pages = [];
       });
-
-    builder.addCase(usersNextPageAsync.pending, (state) => {
-      state.status = 'loading';
-    })
-      .addCase(usersNextPageAsync.fulfilled, (state, action: PayloadAction<UserList>) => {
-        state.status = 'ok';
-        state.pages = action.payload.pages;
-        state.previous = action.payload.previous;
-        state.next = action.payload.next;
-      })
-      .addCase(usersNextPageAsync.rejected, (state, action: PayloadAction<any>) => {
-        state.status = 'failed';
-        state.pages = [];
-      });
   }
-
 });
 
 const selectList = (state: RootState) => state.users;
+
 const selectStatus = (state: RootState) => state.users.status;
+
 export const selectors = {
   selectList, selectStatus
 };
 
 const { clear } = userSlice.actions;
+
 export const actions = {
   clear
 };
