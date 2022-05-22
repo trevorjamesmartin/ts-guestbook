c0324eba939bcdbded6dcd676f3132cb49c2fa93
Trevor Martin <trevor.j.martin@gmail.com>
2022-05-21 20:50:37 -0400

    + thread handlers

--- src/api/posts/index.ts
+++ src/api/posts/index.ts
@@ -1,13 +1,42 @@
 import postsRouter from './posts-router';
-
+import postsModel from './posts-model';
+import userMap from '../common/maps';
+import { verifyToken } from '../auth/restricted-middleware';
+// import { getPage } from '../common/util';
 export { postsRouter }
 
+// *** io -> API ***
+const GET_THREAD = "api:thread"; // eq to GET /api/feed
+// *** API -> io ***
+const RETURN_THREAD = GET_THREAD;
 
-export default (io:any, socket:any) => {
-  const getThread = (token:string, thread_id:number) => {
-      console.log('get Thread:', thread_id, token);
+export default (io: any, socket: any) => {
+  let user = userMap.withSocketId(socket.id);
+  let token = user?.token;
+  let decodedToken: any = undefined;
+  if (token) {
+    decodedToken = verifyToken(token);
   }
 
-  socket.on("api:post", getThread);
+  const getThread = (params: any) => {
+    const { id } = params;
+    console.log('getThread', params)
+    // getPage(decodedToken, postsModel, { id: Number(id) })
+    console.log(GET_THREAD, id);
+    // not paginating threads
+    if (!id) {
+      console.log("id REQUIRED")
+      return
+    }
+
+    postsModel.findByThread(Number(id))
+      .then(result => {
+        // console.log(result);
+        socket.emit(RETURN_THREAD, result);
+      })
+      .catch(console.log)
+  }
+
+  socket.on(GET_THREAD, getThread);
 
 }
