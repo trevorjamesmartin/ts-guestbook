ed5a958960a9b0c840bb0803c3af7fc55442fc62
Trevor Martin <trevor.j.martin@gmail.com>
2022-06-14 13:19:34 -0400

    export component

--- src/app/src/features/index.ts
+++ src/app/src/features/index.ts
@@ -12,6 +12,7 @@ import Profile from './profile/Profile';
 import Navigation from './menu/Navigation';
 import ConnectRequests from './social/Requests';
 import Messenger from './social/Messenger';
+import PublicProfile from './profile/PublicProfile';
 
 import { selectors as authSelectors } from './auth/authSlice';
 import { selectors as socketSelectors } from './network/socketSlice';
@@ -19,7 +20,7 @@ import { selectors as profileSelectors } from './profile/profileSlice';
 import handleIO from './network/config';
 import SocketTest from './network/SocketTest';
 export {
-  Register, Login, Logout, UserList, UserCarousel, UserView,
+  Register, Login, Logout, UserList, UserCarousel, UserView, PublicProfile,
   SocketTest, Thread, Pages, About, Profile, Navigation, ConnectRequests,
   Messenger, handleIO, authSelectors, socketSelectors, profileSelectors
 }
